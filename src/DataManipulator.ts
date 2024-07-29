4491c8c35cd8036286aeb925422609b5443080e7
kalyani harbola <91270628+itskalyaniongithub@users.noreply.github.com>
2024-07-29 10:26:34 +0530

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,31 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
+      timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float'
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverRespond[0], top_ask.price+serverRespond[0].top_bid.price)/2;
+    const priceDEF = (serverRespond[1], top_ask.price+serverRespond[1].top_bid.price)/2;
+    const ratio=priceABC/priceDEF;
+    const upperBound=1+0.05;
+    const lowerBound=1-0.05;
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_abc: priceABC,
+        price_def:priceDEF,
+        ratio,
+        timestamp: serverRespond[0].timestamp>rerverRespond[1].timestamp?
+          serverRespond[0].timestamp:serverRespond[1].timestamp,
+        upper_bounf: upperBound,
+        lower_bound: lowerBound,
+        trigger_alert: (ratio>upperBound||ratio<lowerBound)?ratio: undefined,
       };
-    })
-  }
 }
